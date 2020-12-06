6ce410706eec05e9b732f815b440355f8f9f13c2
shyerLan <shyer.man@qq.com>
2020-12-06 13:04:20 +0800

    master

--- src/components/banner.vue
+++ src/components/banner.vue
@@ -1,54 +1,86 @@
-<template  lang="wxml">
+<template xlang="wxml">
     <view>
-        <swiper :indicator-dots="true">
-            <swiper-item>
-                A
-            </swiper-item>
-            <swiper-item>
-                B
-            </swiper-item>
-        </swiper>
+        <uni-swiper-dot :info="info" :current="current" :mode="mode">
+            <swiper @change="change">
+                <swiper-item v-for="item in info" :key="item">
+                    <view class="big l">
+                        <text>{{ item.banner1 }}</text>
+                    </view>
+                    <view class="big r">
+                        <view class="small">
+                            <text>{{ item.banner2 }}</text>
+                        </view>
+                        <view class="small">
+                            <text>{{ item.banner3 }}</text>
+                        </view>
+                    </view>
+                </swiper-item>
+            </swiper>
+        </uni-swiper-dot>
     </view>
 </template>
 
 <script>
-    export default {
-        name: '',
-        components: {
+import { uniSwiperDot } from "@dcloudio/uni-ui";
+export default {
+    name: "",
+    components: {
+        uniSwiperDot,
+    },
+    props: {},
+    data() {
+        return {
+            info: [
+                {
+                    banner1: "内容 A1",
+                    banner2: "内容 A2",
+                    banner3: "内容 A3",
+                },
+                {
+                    banner1: "内容 B1",
+                    banner2: "内容 B2",
+                    banner3: "内容 B3",
+                },
+                {
+                    banner1: "内容 C1",
+                    banner2: "内容 C2",
+                    banner3: "内容 C3",
+                },
+            ],
+            current: 0,
+            mode: "round",
+        };
+    },
+    computed: {},
+    watch: {},
+    created() {},
+    mounted() {},
+    methods: {
+        change(e) {
+            this.current = e.detail.current;
         },
-        props: {
-
-        },
-        data() {
-            return {
-            };
-        },
-        computed: {
-
-        },
-        watch: {
-
-        },
-        created() {
-
-        },
-        mounted() {
-
-        },
-        methods: {
-            change(e) {
-                this.current = e.detail.current;
-            }
-        },
-    };
+    },
+};
 </script>
 
 <style lang="scss">
-    swiper {
-		width: 750rpx;
-		height: 300rpx;
+swiper {
+    width: 750rpx;
+    height: 300rpx;
+}
+swiper-item {
+    display: flex;
+}
+.big {
+    height: 100%;
+}
+.l {
+    width: 500rpx;
+}
+.r {
+    width: 250rpx;
+    .small {
+        height: 50%;
     }
-    image {
-        height: 100%;
-    }
+}
 </style>
--- src/pages/index/index.vue
+++ src/pages/index/index.vue
@@ -1,42 +1,37 @@
 <template lang="wxml">
 	<view class="content">
-		<view class="banner">
 			<me-banner></me-banner>
-		</view>
 	</view>
 </template>
 
 <script>
-	import meBanner from "@/components/banner.vue"
-	export default {
-		components: {
-			meBanner
-		},
-		data() {
-			return {
-			}
-		},
-		onLoad() {
-
-		},
-		methods: {
-
-		}
-	}
+import meBanner from "@/components/banner.vue";
+export default {
+    components: {
+        meBanner,
+    },
+    data() {
+        return {};
+    },
+    onLoad() {},
+    methods: {},
+};
 </script>
 
 <style lang="scss">
-	page{
-		width: 100vw;
-		height: 100vh;
-	}
-	.content {
-		display: flex;
-		flex-direction: column;
-		align-items: center;
-		justify-content: center;
-	}
-	.banner{
-		background-color: antiquewhite;
-	}
+page {
+    width: 100vw;
+    height: 100vh;
+    background-color: #f5f5f5;
+}
+.content {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+}
+me-banner{
+	margin: 10px 0;
+	background-color: #fff;
+}
 </style>
